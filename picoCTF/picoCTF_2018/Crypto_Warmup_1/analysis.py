8bdbdb87db8385e784e1aaa7fa28fd82fe048226
Sd9ToU <46506833+Sd9ToU@users.noreply.github.com>
2020-04-10 11:19:33 +0900

    Delete analysis.py

--- picoCTF/picoCTF_2018/Crypto_Warmup_1/analysis.py
+++
@@ -1,8 +0,0 @@
-ciphertext = "llkjmlmpadkkc"
-key = "thisisalilkey"
-
-# table
-for i in range(26):
-    for ch in range(ord('A'), ord('Z') + 1):
-        print(chr((ch - ord('A') + i) % 26 + ord('A')), end='')
-    print()
