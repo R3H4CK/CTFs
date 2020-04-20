47966c8f718863b008ddec1b1733de28addd2fa4
Sd9ToU <46506833+Sd9ToU@users.noreply.github.com>
2020-04-20 16:53:48 +0900

    Create analysis.py

---
+++ picoCTF/picoCTF_2019/Reverse_Engineering/vault-door-3/analysis.py
@@ -0,0 +1,20 @@
+flag = "jU5t_a_sna_3lpm13gc49_u_4_m0rf41"
+
+print(len(flag))
+print(flag)
+
+for i in range(0, 8):
+    print(i, end=' ')
+print()
+
+for i in range(i + 1, 16):
+    print(23 - i, end=' ')
+print()
+
+for i in range(i + 1, 32, 2):
+    print(46 - i, end=' ')
+print()
+
+for i in range(31, 17, -2):
+    print(i, end=' ')
+print()
