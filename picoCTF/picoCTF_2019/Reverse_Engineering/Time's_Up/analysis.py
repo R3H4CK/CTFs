507091e4a26d659da5d6e6d81483f1da72199e23
Sd9ToU <46506833+Sd9ToU@users.noreply.github.com>
2020-04-21 20:40:40 +0900

    Update analysis.py

--- picoCTF/picoCTF_2019/Reverse_Engineering/Time's_Up/analysis.py
+++ picoCTF/picoCTF_2019/Reverse_Engineering/Time's_Up/analysis.py
@@ -1,25 +1,23 @@
 from ctypes import *
-import signal
-import os
 
-msvcrt = CDLL("msvcrt.dll")
+libc = CDLL("libc.so.6")
 
-seed = c_longlong()
-result = c_longlong()
-guess = c_longlong()
+seed = c_int()
+result = c_int()
+guess = c_int()
 
 def init_randomness():
-    msvcrt.srand(msvcrt.time(None))
+    libc.srand(libc.time(None))
     global seed
-    seed.value = msvcrt.rand() * msvcrt.rand() + msvcrt.rand()
+    seed.value = libc.rand() * libc.rand() + libc.rand()
 
 def get_random():
     global seed
-    seed.value *= msvcrt.rand()
-    seed.value += msvcrt.rand()
+    seed.value *= libc.rand()
+    seed.value += libc.rand()
     seed.value *= 1337
-    seed.value += msvcrt.rand()
-    return c_longlong(msvcrt.rand() * seed.value)
+    seed.value += libc.rand()
+    return c_int(libc.rand() * seed.value)
 
 def get_random_op():
     v1 = 11563
@@ -27,29 +25,29 @@ def get_random_op():
 
 def do_op(a, b, c):
     if a == 43:
-        return c_longlong(b + c)
+        return c_int(b + c)
     if a != 45:
         exit(1)
-    return c_longlong(b - c)
+    return c_int(b - c)
 
 def maybe_decrease(n):
-    return c_int(n) if msvcrt.rand() % 50 <= 0 else c_int(n - 1)
+    return c_int(n) if libc.rand() % 50 <= 0 else c_int(n - 1)
 
 def gen_expr(n):
     if n:
         v3 = maybe_decrease(n).value
         v4 = maybe_decrease(n).value
         v5 = get_random_op().value
-        print(chr(40), end='')
+        print('(', end='')
         v6 = gen_expr(v3).value
-        print(f" {chr(v5)} ", end='')
+        print(" %c " % chr(v5), end='')
         v7 = gen_expr(v4).value
-        print(chr(41), end='')
+        print(')', end='')
         return do_op(v5, v6, v7)
     else:
         v1 = get_random().value
         print("%d" % v1, end='')
-        return c_longlong(v1)
+        return c_int(v1)
 
 def generate_challenge():
     global result
@@ -61,12 +59,12 @@ print("Challenge: ", end='')
 generate_challenge()
 print(chr(10), end='', flush=True)
 print("Setting alarm...", flush=True)
-signal.alarm(5)
+libc.alarm(5)
 print("Solution? ", end='')
-msvcrt.scanf(b"%lld", pointer(guess))
+libc.scanf(b"%lld", pointer(guess))
 
 if guess.value == result.value:
     print("Congrats! Here is the flag!")
-    os.system("/bin/cat flag.txt")
+    libc.system("/bin/cat flag.txt")
 else:
     print("Nope!")
